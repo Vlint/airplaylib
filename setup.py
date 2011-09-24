c0d6d801e6df8b60e8e89a19177f79d7b37548d2
Trevor Harwell <trevor.harwell@gmail.com>
2011-09-23 17:17:38 -0700

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import airplaylib
+
+setup(name='airplaylib',
+	version=airplaylib.__version__,
+	description='Airplay libraries for Python',
+	author='Trevor Harwell',
+	author_email='trevor.harwell@gmail.com',
+	py_modules=['airplaylib'])
